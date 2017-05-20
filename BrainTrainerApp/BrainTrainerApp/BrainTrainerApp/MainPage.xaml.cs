bcc2fc06baf61f176c6d086b1336d51baf66a00b
ThomasWiens <thwiens@outlook.de>
2017-05-20 13:12:22 +0200

    socket.io socket.io eingebunden und eine Testverbindung hergestellt

--- BrainTrainerApp/BrainTrainerApp/BrainTrainerApp/MainPage.xaml.cs
+++ BrainTrainerApp/BrainTrainerApp/BrainTrainerApp/MainPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Quobject.SocketIoClientDotNet.Client;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,23 @@ namespace BrainTrainerApp
 		public MainPage()
 		{
 			InitializeComponent();
+            ConnectToSocket();
 		}
+
+        // https://github.com/Quobject/SocketIoClientDotNet/
+        private void ConnectToSocket()
+        {
+            var socket = IO.Socket("ws://localhost:8080");
+
+            socket.On(Socket.EVENT_CONNECT, () =>
+            {
+                Debug.WriteLine("Connect");
+            });
+
+            socket.On(Socket.EVENT_MESSAGE, (data) =>
+            {
+                Debug.WriteLine(data);
+            });
+        }
 	}
 }
