0680f0cddf71afa0563ad0647377ed4ad53359b6
Markus Wiebe <markuswiebe@web.de>
2017-07-09 18:46:51 +0200

    Der letzte Stand von Xamarin

---
+++ BrainTrainerApp/BrainTrainerApp/BrainTrainerApp.Android/MyFirebaseMessagingService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Media;
+using Android.Util;
+using Firebase.Messaging;
+
+namespace TestApp.Droid
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class BraintrainerFirebaseMessagingService : FirebaseMessagingService
+    {
+        const string TAG = "BraintrainerFirebaseMessagingService";
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            Log.Debug(TAG, "From: " + message.From);
+            Log.Debug(TAG, "Notification Message Body: " + message.GetNotification().Body);
+            SendNotification(message.GetNotification().Body);
+        }
+
+        void SendNotification(string messageBody)
+        {
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.AddFlags(ActivityFlags.ClearTop);
+            var pendingIntent = PendingIntent.GetActivity(this, 0, intent, PendingIntentFlags.OneShot);
+
+            var notificationBuilder = new Notification.Builder(this)
+                .SetSmallIcon(Resource.Drawable.Icon)
+                .SetContentTitle("Jetzt üben")
+                .SetContentText(messageBody)
+                .SetAutoCancel(true)
+                .SetContentIntent(pendingIntent);
+
+            var notificationManager = NotificationManager.FromContext(this);
+            notificationManager.Notify(0, notificationBuilder.Build());
+        }
+    }
+}
--- BrainTrainerApp/BrainTrainerApp/BrainTrainerApp/MainPage.xaml.cs
+++ BrainTrainerApp/BrainTrainerApp/BrainTrainerApp/MainPage.xaml.cs
@@ -12,17 +12,39 @@ namespace BrainTrainerApp
 {
 	public partial class MainPage : ContentPage
 	{
+        private WebView webview;
+        private string token;
+
 		public MainPage()
 		{
 			InitializeComponent();
-            ConnectToSocket();
-            //LocalNotification();
+            webview = this.FindByName<WebView>("WebView");
+            webview.HorizontalOptions = LayoutOptions.Fill;
+            webview.VerticalOptions = LayoutOptions.Fill;
+            //ConnectToSocket();
 		}
+
+        protected override bool OnBackButtonPressed()
+        {
+            Debug.WriteLine(webview.Source);
+            //webview.Eval("console.log(document.body.innerHTML)");
+            if (webview.CanGoBack)
+            {
+                webview.GoBack();
+                return true;
+            }
+            return base.OnBackButtonPressed();
+        }
 
         // https://github.com/Quobject/SocketIoClientDotNet/
         private void ConnectToSocket()
         {
             var socket = IO.Socket("https://braintrainer.herokuapp.com/");
+            var option = new IO.Options();
+            var headers = new Dictionary<string, string>();
+            headers.Add("Authorization", "Bearer " + token);
+            option.ExtraHeaders = headers;
+            IO.Socket("https://braintrainer.herokuapp.com/");
 
             socket.On(Socket.EVENT_CONNECT, () =>
             {
@@ -39,7 +61,6 @@ namespace BrainTrainerApp
         // https://github.com/edsnider/LocalNotificationsPlugin
         private void LocalNotification()
         {
-
             CrossLocalNotifications.Current.Show("title", "body");
             Debug.WriteLine("Done");
         }
