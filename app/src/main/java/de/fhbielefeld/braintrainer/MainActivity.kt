095b7e1ed65b121c456368dab2b0c9afdbb38598
Markus Wiebe <markuswiebe@web.de>
2017-07-09 20:57:51 +0200

    Richtiges App Icon eingestellt und webview Member in MainActivity gefixt

--- app/src/main/java/de/fhbielefeld/braintrainer/MainActivity.kt
+++ app/src/main/java/de/fhbielefeld/braintrainer/MainActivity.kt
@@ -8,18 +8,23 @@ import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
 
-    private var webview = findViewById<WebView>(R.id.webview)
+    private var webview: WebView?
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        webview.settings.javaScriptEnabled = true
+        webview = findViewById<WebView>(R.id.webview)
+        webview?.settings?.javaScriptEnabled = true
         if (Build.VERSION.SDK_INT >= 21) {
-            webview.settings.mixedContentMode = WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE
-            webview.loadUrl(getString(R.string.braintrainerSecureURL))
+            webview?.settings?.mixedContentMode = WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE
+            webview?.loadUrl(getString(R.string.braintrainerSecureURL))
         } else {
-            webview.loadUrl(getString(R.string.braintrainerURL))
+            webview?.loadUrl(getString(R.string.braintrainerURL))
         }
+    }
+
+    init {
+        webview = null
     }
 }
