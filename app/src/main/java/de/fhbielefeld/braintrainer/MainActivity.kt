3dd3148e7390e4ec380d0c2e9fb8cbcfbf7b682a
Markus Wiebe <markuswiebe@web.de>
2017-07-09 19:26:15 +0200

    Website von Heroku laden

--- app/src/main/java/de/fhbielefeld/braintrainer/MainActivity.kt
+++ app/src/main/java/de/fhbielefeld/braintrainer/MainActivity.kt
@@ -1,12 +1,25 @@
 package de.fhbielefeld.braintrainer
 
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
+
+    private var webview = findViewById<WebView>(R.id.webview)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        webview.settings.javaScriptEnabled = true
+        if (Build.VERSION.SDK_INT >= 21) {
+            webview.settings.mixedContentMode = WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE
+            webview.loadUrl(getString(R.string.braintrainerSecureURL))
+        } else {
+            webview.loadUrl(getString(R.string.braintrainerURL))
+        }
     }
 }
